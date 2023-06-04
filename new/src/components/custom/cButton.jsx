d42b91faade19eeaced9a5d6a6057f94a50010ce
sopa301 <96387349+sopa301@users.noreply.github.com>
2023-06-05 03:01:57 +0800

    added some spinny things

---
+++ new/src/components/custom/cButton.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@chakra-ui/react";
+import { useState } from "react";
+
+export default function CButton(props) {
+    // This button is only for handling async functions
+    const [loading, setLoading] = useState(false);
+    return <Button
+        isLoading={loading}
+        onClick={() => {
+            setLoading(true);
+            props.onClick().then(() => setLoading(false));
+        }}>
+            {props.content}
+        </Button>
+}
