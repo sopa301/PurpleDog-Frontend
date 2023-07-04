113d9524b94e638eeeed70c8adce1ae2bc72573f
sopa301 <96387349+sopa301@users.noreply.github.com>
2023-07-04 12:54:20 +0800

    optimise tests

--- new/__test__/components/projects/manageTasks/taskMenu.test.jsx
+++ new/__test__/components/projects/manageTasks/taskMenu.test.jsx
@@ -1,59 +1,31 @@
-import React, { createContext } from "react";
-import { render, fireEvent, cleanup, act } from "@testing-library/react";
-import {
-  ChakraProvider,
-  extendTheme as chakraExtendTheme,
-  createStandaloneToast,
-} from "@chakra-ui/react";
-import {
-  ThemeProvider as MaterialThemeProvider,
-  createTheme as muiCreateTheme,
-  THEME_ID,
-} from "@mui/material/styles";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
+import React from "react";
+import { render } from "../../../test_utils";
 import { BrowserRouter } from "react-router-dom";
-import { DateTime, Interval } from "luxon";
+import { DateTime } from "luxon";
 import { Project } from "../../../../src/objects/Project";
 import TaskMenu from "../../../../src/components/projects/manageTasks/taskMenu";
-
-const materialTheme = muiCreateTheme();
-const chakraTheme = chakraExtendTheme();
-const { ToastContainer, toast } = createStandaloneToast();
-const ToastContext = createContext(toast);
 
 test("If it renders properly", () => {
   const startTime = new DateTime(1000, 1, 1);
   const endTime = new DateTime(2020, 2, 2);
   const compo = render(
-    <div>
-      <MaterialThemeProvider theme={{ [THEME_ID]: materialTheme }}>
-        <LocalizationProvider dateAdapter={AdapterLuxon}>
-          <ToastContext.Provider value={toast}>
-            <BrowserRouter>
-              <ChakraProvider theme={chakraTheme} resetCSS>
-                <TaskMenu
-                  isOpen={true}
-                  title="title"
-                  proj={new Project(1, "Project", [], [])}
-                  initialValues={{
-                    name: "name1",
-                    pax: 3,
-                    priority: 2,
-                    start: startTime,
-                    end: endTime,
-                    assignees: [null, null, null],
-                    completed: false,
-                  }}
-                  onSubmit={() => {}}
-                />
-              </ChakraProvider>
-            </BrowserRouter>
-          </ToastContext.Provider>
-          <ToastContainer />
-        </LocalizationProvider>
-      </MaterialThemeProvider>
-    </div>
+    <BrowserRouter>
+      <TaskMenu
+        isOpen={true}
+        title="title"
+        proj={new Project(1, "Project", [], [])}
+        initialValues={{
+          name: "name1",
+          pax: 3,
+          priority: 2,
+          start: startTime,
+          end: endTime,
+          assignees: [null, null, null],
+          completed: false,
+        }}
+        onSubmit={() => {}}
+      />
+    </BrowserRouter>
   );
   expect(compo.queryAllByText("title")).toHaveLength(2);
   expect(compo.queryByText("Task Name")).toBeTruthy();
@@ -81,5 +53,4 @@ test("If it renders properly", () => {
         endTime.toLocaleString(DateTime.TIME_SIMPLE)
     )
   ).toBeTruthy();
-  
 });
--- new/__test__/components/projects/manageTasks/taskSettings.test.jsx
+++ new/__test__/components/projects/manageTasks/taskSettings.test.jsx
@@ -1,22 +1,13 @@
-import React, { createContext } from "react";
-import { render, fireEvent, cleanup, act } from "@testing-library/react";
+import React from "react";
+import { render } from "../../../test_utils";
 import TaskSettings from "../../../../src/components/projects/manageTasks/taskSettings";
-import {
-  Accordion,
-  ChakraProvider,
-  extendTheme as chakraExtendTheme,
-  createStandaloneToast,
-} from "@chakra-ui/react";
+import { Accordion } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { TaskGroup } from "../../../../src/objects/TaskGroup";
 import { Task } from "../../../../src/objects/Task";
 import { DateTime, Interval } from "luxon";
 import { Person } from "../../../../src/objects/Person";
 import { Project } from "../../../../src/objects/Project";
-
-const chakraTheme = chakraExtendTheme();
-const { ToastContainer, toast } = createStandaloneToast();
-const ToastContext = createContext(toast);
 
 test("If it renders properly", () => {
   const taskGroup = new TaskGroup(
@@ -37,30 +28,23 @@ test("If it renders properly", () => {
     1
   );
   const compo = render(
-    <div>
-      <ToastContext.Provider value={toast}>
-        <BrowserRouter>
-          <ChakraProvider theme={chakraTheme} resetCSS>
-            <Accordion>
-              <TaskSettings
-                taskGroup={taskGroup}
-                index={0}
-                update={() => {}}
-                proj={
-                  new Project(
-                    1,
-                    "Project",
-                    [new Person(1, "Spiderman Lotus", [], "viewer")],
-                    [taskGroup]
-                  )
-                }
-              />
-            </Accordion>
-          </ChakraProvider>
-        </BrowserRouter>
-      </ToastContext.Provider>
-      <ToastContainer />
-    </div>
+    <BrowserRouter>
+      <Accordion>
+        <TaskSettings
+          taskGroup={taskGroup}
+          index={0}
+          update={() => {}}
+          proj={
+            new Project(
+              1,
+              "Project",
+              [new Person(1, "Spiderman Lotus", [], "viewer")],
+              [taskGroup]
+            )
+          }
+        />
+      </Accordion>
+    </BrowserRouter>
   );
   expect(compo.queryByText("Ohio")).toBeTruthy();
 });
